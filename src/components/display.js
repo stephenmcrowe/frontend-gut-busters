79b52b8f3f6c6528ec6b4d3d389d0d7493b89cd7
Charlotte C <hao.qing.charlotte.chui.20@dartmouth.edu>
2019-05-13 17:21:03 -0400

    displays hello world on the frontend

--- src/components/display.js
+++ src/components/display.js
@@ -1,1 +1,11 @@
 // literally just display everything
+import React from 'react';
+
+const Display = (props) => {
+  return (
+    <div className="displayWrapper"> <h1> Hello world!</h1>
+    </div>
+  );
+};
+
+export default Display;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,22 @@
 import { combineReducers } from 'redux';
-import DisplayReducer from './display-reducer';
-import VoteReducer from './vote-reducer';
-import SubmitReducer from './submit-reducer';
+// import DisplayReducer from './display-reducer';
+// import VoteReducer from './vote-reducer';
+// import SubmitReducer from './submit-reducer';
+
+// const rootReducer = combineReducers({
+//   display: DisplayReducer,
+//   votes: VoteReducer,
+//   submit: SubmitReducer,
+// });
+
+// export default rootReducer;
+
+// the starting point for your redux store
+// this defines what your store state will look like
+import CountReducer from './count-reducer';
 
 const rootReducer = combineReducers({
-  display: DisplayReducer,
-  votes: VoteReducer,
-  submit: SubmitReducer,
+  count: CountReducer,
 });
 
 export default rootReducer;
