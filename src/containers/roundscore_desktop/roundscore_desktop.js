bf379b0c4ff14eb42ebec421ff6382b8eaf500b6
danielle-fang <danielle.fang.20@dartmouth.edu>
2019-05-23 14:41:56 -0400

    finished roundscore

--- src/containers/roundscore_desktop/roundscore_desktop.js
+++ src/containers/roundscore_desktop/roundscore_desktop.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 //  import { endRounds } from '../../actions';
-// import './roundscore_desktop.scss';
+import './roundscore_desktop.scss';
 
 // Data needed (receive):
 // props.player array with each player having player.score from round
