9fa7f289141875d315b1abbc5ff1c7f2132f0fbf
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-06-03 02:07:09 -0400

    scoring created for score_mobile

--- src/containers/score_mobile/score_mobile.js
+++ src/containers/score_mobile/score_mobile.js
@@ -12,35 +12,79 @@ import SocketContext from '../../socket-context';
 // Build ranking logic function here
 
 // this can be dumb or smart component
+// class MobileScore extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
 const MobileScore = (props) => {
-  return (
-    <div className="score-page">
-      <img className="score-icon" src={ghost} alt="Ghost Icon" />
+  function calcScores() {
+    const scores = props.game.players.reduce((map, curr) => {
+      // eslint-disable-next-line no-param-reassign
+      map[curr._id] = 0;
+      return map;
+    }, {});
+
+    props.game.questions.forEach((question) => {
+      question.answers.forEach((answer) => {
+        scores[answer.player] += answer.score;
+      });
+    });
+
+    // https://stackoverflow.com/questions/25500316/sort-a-dictionary-by-value-in-javascript
+    const sortedScores = Object.keys(scores).map((key) => {
+      return [key, scores[key]];
+    });
+    // Sort the array based on the second element
+    sortedScores.sort((first, second) => {
+      return second[1] - first[1];
+    });
+
+    const finalScores = {};
+    sortedScores.forEach((item, idx) => {
+      finalScores[item[0]] = {
+        score: item[1],
+        rank: idx + 1,
+      };
+    });
+
+    console.log(finalScores);
+    return finalScores;
+  }
+
+  function renderScore() {
+    const scores = calcScores();
+    return (
       <div className="score-text">
         <div className="final-score">
           <h1>YOU WON!</h1>
-          {/* replace with if depending on ranking */}
+          {/* Change as appropriate... for now, everyone is a winner */}
           <h2>Final Score</h2>
-          <h3>104</h3>
-          {/* props.player.score */}
+          <h3>{scores[localStorage.getItem('myId')].score * 100}</h3>
         </div>
         <div className="rank">
           <h2>Rank</h2>
-          <h3>2</h3>
-          {/* put ranking logic function here */}
+          <h3>{scores[localStorage.getItem('myId')].rank}</h3>
         </div>
       </div>
+    );
+  }
+
+  return (
+    <div className="score-page">
+      <img className="score-icon" src={ghost} alt="Ghost Icon" />
+      {renderScore()}
     </div>
 
   );
 };
 
 // connects particular parts of redux state to this components props
-const mapStateToProps = state => (
-  {
-    player: state.player,
-  }
-);
+function mapStateToProps(reduxState) {
+  return {
+    game: reduxState.socket.game,
+  };
+}
 
 const MobileScoreWithSocket = props => (
   <SocketContext.Consumer>
